a6c908d32ef5ea43dd5d7e2f0844d40cc8fcbdae
Alenna Castaneda <alenna.castaneda@oneidaihla.org>
2022-09-05 07:42:35 -0600

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+      for (int x = 1; x < 101; x++) {
+        if (x%2 == 0) {
+          System.out.println(x + " is even.");
+          }
+          else {
+            System.out.println(x + " is odd.");
+          }
+        }
+      }
+    }
+  
